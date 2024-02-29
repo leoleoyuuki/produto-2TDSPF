8c4631dca994583ab0ae317df646b2fcf1469cb3
leoleoyuuki <rm550373@fiap.com.br>
2024-02-29 11:27:02 -0300

    ATUALIZACAO LOMBOK

--- src/main/java/br/com/fiap/Opcional.java
+++ src/main/java/br/com/fiap/Opcional.java
@@ -1,7 +1,16 @@
 package br.com.fiap;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Table(name = "TB_2TDSPF_OPCIONAL")
 public class Opcional {
 
@@ -18,49 +27,6 @@ public class Opcional {
     private Double preco;
 
 
-        public Opcional(){
 
-        }
 
-    public Opcional(Long id, String nome, Double preco) {
-        this.id = id;
-        this.nome = nome;
-        this.preco = preco;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Opcional setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public Opcional setNome(String nome) {
-        this.nome = nome;
-        return this;
-    }
-
-    public Double getPreco() {
-        return preco;
-    }
-
-    public Opcional setPreco(Double preco) {
-        this.preco = preco;
-        return this;
-    }
-
-    @Override
-    public String toString() {
-        return "Opcional{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", preco=" + preco +
-                '}';
-    }
 }
